691a3cd233fd9d34b276e3cd081d883e59b591c6
Sal Tamay <sal.tamay@outlook.com>
2020-02-20 00:51:48 -0500

    bug: Update migrations env file with env variables

--- migrations/env.py
+++ migrations/env.py
@@ -28,7 +28,7 @@ load_dotenv()
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
 config.set_main_option(
     'sqlalchemy.url', current_app.config.get(
-        'SQLALCHEMY_DATABASE_URI').replace('%', '%%'))
+        'SQLALCHEMY_DATABASE_URI'))
 target_metadata = current_app.extensions['migrate'].db.metadata
 
 # other values from the config, defined by the needs of env.py,
