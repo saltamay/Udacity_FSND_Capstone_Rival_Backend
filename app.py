69850ae6a573a5272c8d913000735e3fb8eac3a5
Sal Tamay <sal.tamay@outlook.com>
2020-02-20 02:17:38 -0500

    bug: remove db.create_all(), using migrations scripts

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv(
     'SQLALCHEMY_TRACK_MODIFICATIONS')
 db.app = app
 db.init_app(app)
-db.create_all()
+# db.create_all()
 
 migrate = Migrate(app, db)
 CORS(app)
